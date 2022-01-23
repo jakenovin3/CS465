a9d2d5695d7fdeff66d92169fb91b2678fd0e322
vidalrmartinez <vrm55@nau.edu>
2022-01-23 11:53:04 -0700

    updated constructor to cast content and return object.

--- Message.java
+++ Message.java
@@ -2,13 +2,17 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     // The object streams need to be serializable
-    public Message( int messageType, Object content ) {
+    public Message(){};
+    public Object Message( MessageTypes messageType, Object content ) {
         // determine type of message
-        // cast content to appropriate type
-    }
-    // getter to provide message as whatever type appropriate
-    public String getMessage() {
-        return "";
+        if( messageType == MessageTypes.JOIN
+            || messageType == MessageTypes.LEAVE
+            || messageType == MessageTypes.SHUTDOWN ) {
+            NodeInfo participantInfo = (NodeInfo) content;
+            return participantInfo;
+        }
+        String participantNote = (String) content;
+        return participantNote;
     }
 
 }
