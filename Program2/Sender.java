f0a8771fc22c75909ef9af8d821afefdb4f4b0b4
Jake <jakenovin3>
2022-02-25 16:28:58 -0700

    Checkpoint. Almost working chat

--- Program2/Sender.java
+++ Program2/Sender.java
@@ -7,14 +7,6 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 // new joining person connects off of some one person's IP
 // that participant adds joiner's info to their own list
