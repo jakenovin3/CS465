6eaf21af28697ce43bfe11cf0f6ce81027828753
Vidal Martinez <50600533+vidalrmartinez@users.noreply.github.com>
2022-03-16 01:18:53 -0700

    added functions

--- Program3/AccountManager.java
+++ Program3/AccountManager.java
@@ -1,9 +1,16 @@
 public class AccountManager {
     // DESCRIPTION:
-
     // Manages Accounts that can be read and written to
-
     // Here, it implements the "high-level" operations on individual accounts (read and write)
+    
+    // METHODS:
+    // read() function:
+    // given an account number, access select account and read balance of the account
+    // return value
+    
+    // write() function:
+    // given an account number, access select account and send new balance to the account to update
+    // return void
 
     //------------------------------------------------------------
     // USE CASE:
